5402c39d0de6659a02892f23749e1fd0f4324909
darkvadehors <johnben.dev@gmail.com>
2021-03-16 16:19:06 +0100

    add schemas in module

--- src/app/features/login/login.modules.ts
+++ src/app/features/login/login.modules.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { LoginComponent } from './container/components/login/login.component';
@@ -11,6 +11,7 @@ import { LoginComponent } from './container/components/login/login.component';
         CommonModule,
         FormsModule,
         ReactiveFormsModule,
-    ]
+    ],
+    schemas: [ CUSTOM_ELEMENTS_SCHEMA ]
 })
 export class LoginModule { }
